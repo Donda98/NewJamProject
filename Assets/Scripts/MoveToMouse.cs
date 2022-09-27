5fac2cf3f1d342be3f5d8c5b0003014f0451ec25
LEONIDA <dadonda98@hotmail.com>
2022-09-27 17:00:19 +0200

    Update Movement & Click

--- Assets/Scripts/MoveToMouse.cs
+++ Assets/Scripts/MoveToMouse.cs
@@ -6,49 +6,52 @@ public class MoveToMouse : MonoBehaviour
 {
     public float speed = 5f;
     public LayerMask layersToRay;
+    public LayerMask itemLayer;
 
     [SerializeField] private Camera mainCam;
 
+    private bool mouseOnItem;
     private Vector3 target;
-    private Ray cameraRay;
-    private GameObject objectHit;
+    private GameObject itemHit;
 
-    // Start is called before the first frame update
     void Start()
     {
         target = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition),out RaycastHit hit_01, 500f, itemLayer))
+        {
+            mouseOnItem = true;
+            itemHit = hit_01.collider.gameObject;
+            print("Sono sopra un Item");
+        }
+        else
+        {
+            mouseOnItem = false;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
-           GameManager.Instance.mixerAudio.PlayOneShot(GameManager.Instance.UIAudio[0]);
-           Ray cameraRay = mainCam.ScreenPointToRay(Input.mousePosition);
-           if (Physics.Raycast(cameraRay,out RaycastHit hit,layersToRay))
-           {
-                objectHit = hit.collider.gameObject;
-                if(objectHit.TryGetComponent(out InteractableLEO interactableObjectHit) == true)
+            if (mouseOnItem == false)
+            {
+                GameManager.Instance.mixerAudio.PlayOneShot(GameManager.Instance.UIAudio[0]);
+                Ray cameraRay = mainCam.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(cameraRay, out RaycastHit hit, 500f, layersToRay))
                 {
-                    /*  switch(interactableObjectHit)
-                      {
-
-                      }*/
-                    print($"Mi muovo verso:{target.x}");
                     target = hit.point;
                 }
                 else
                 {
                     print("Non posso andarci!");
+
                 }
-           }
-           else
-           {
-                print("Non posso andarci!");
-
-           }
-
+            }
+            else
+            {
+                print("Ho cliccato un Item");
+            }
            target.z = transform.position.z;
            target.y = transform.position.y;
         }
