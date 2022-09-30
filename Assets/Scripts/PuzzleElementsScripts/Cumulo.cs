a706822ac3942366d17080412ac313788afc8042
Grothgar <97904583+Grothgar@users.noreply.github.com>
2022-09-30 21:51:47 +0200

    NPC and Item updated

---
+++ Assets/Scripts/PuzzleElementsScripts/Cumulo.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cumulo : PuzzleElement
+{
+    [SerializeField] Cumulo[] altriCumuli = { null, null };
+    [SerializeField] GameObject robot;
+    int cumuliRestanti;
+
+    private void Awake()
+    {
+        cumuliRestanti = altriCumuli.Length;
+    }
+
+    public override void CustomOnClickAction(Inventory playerInventory)
+    {
+        print("I cumuli");
+        if(cumuliRestanti == 0)
+        {
+           GameObject spawnRobot = Instantiate(robot, this.transform.position, Quaternion.identity);
+           playerInventory.FreeInventorySlot();
+        }
+        else
+        {
+            for (int i = 0; i < altriCumuli.Length; i++)
+            {
+                altriCumuli[i].riduciNumeroCumuli();
+            }
+        }
+        Destroy(this.gameObject);
+    }
+
+    public void riduciNumeroCumuli()
+    {
+        cumuliRestanti--;
+    }
+
+    
+}
